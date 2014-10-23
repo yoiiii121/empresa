08efdefe13155d494e40813f89490c0de8b522ee
N-W7 <yoiiii121@gmail.com>
2014-10-23 22:52:22 +0200

    typewriting error fixed

--- Empresa.py
+++ Empresa.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Nacho-w7'
 
-class Empresas():
+class Empresa():
     def __init__(self,nombre_empresa, cif,razon_social):
         self.__nombre_empresa=nombre_empresa
         self.__cif=cif
