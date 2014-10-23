18bbaa08883508716a2ec2f8a06bc178c4bcf479
N-W7 <yoiiii121@gmail.com>
2014-10-23 22:35:57 +0200

    new class

---
+++ Empresa.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Nacho-w7'
+
+class Empresas():
+    def __init__(self,nombre_empresa, cif,razon_social):
+        self.__nombre_empresa=nombre_empresa
+        self.__cif=cif
+        self.__razon_social=razon_social
+        self.__departamentos=[]
